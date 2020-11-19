217d23527e82ff59374568802d1c4e0ef52ba2f9
Jake Tan <yoongkai@gmail.com>
2020-11-20 05:51:52 +0800

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const quoteDisplay = document.querySelector('#quote')
+const button = document.querySelector('button')
+
+//Challenge: Write a function that will display a random Inspirational Quote
+//from the quotes array, when the button is pressed.
+const quotes = [
+    'Do something today that your future self will thankyou for',
+    'Dont worry, be happy!',
+    'Your limitation is only your imagination',
+    'Either you run the day, or the day runs you'
+]
+
+function showQuote() {
+    //your code
+    let randomNumber = Math.floor(Math.random() * quotes.length)
+    let randomQuote = quotes[randomNumber]
+    quoteDisplay.innerHTML = randomQuote
+}
+
+button.addEventListener('click', showQuote)
